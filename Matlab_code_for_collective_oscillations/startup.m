440d1d628e45e2739c74321792538f3267a57ae7
Shouwen Wang <wangsw09@gmail.com>
2019-09-14 13:40:36 -0400

    Include glysolysis fild

--- Matlab_code_for_collective_oscillations/startup.m
+++ Matlab_code_for_collective_oscillations/startup.m
@@ -1,5 +1,6 @@
 addpath('./functions/');
 addpath('./My_Common_Function');
 addpath('./adaptation/functions/');
-addpath('./FitzHugh-Nagumo Model/functions/');
+addpath('./FitzHugh-Nagumo_Model/functions/');
+addpath('./glycolysis/functions/');
 
